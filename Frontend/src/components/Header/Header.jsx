97619a9b63ed95b45e53a42ce46a92859fa00c3a
renatajane <renatajane28@gmail.com>
2024-11-09 14:41:38 -0300

    feat: inserir links no header

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import './Header.css'; 
+import './Header.css';
 import logo from '../../assets/logo.png';
 
 function Header() {
     return (
         <header className="header">
             <div className="header-content">
-            <img src={logo} alt="Logo PetAdota" className="logo" />
-                {/* <h1>PetAdota</h1> */}
-                <nav className="header-nav">
-                    {/* <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">Sobre</a></li>
-                        <li><a href="/pets">Pets para Adoção</a></li>
-                        <li><a href="/contact">Contato</a></li>
-                    </ul> */}
-                </nav>
+                <img src={logo} alt="Logo PetAdota" className="logo" />
             </div>
+            <nav className="header-nav">
+                <ul>
+                    <li><a href="/"><strong>Home</strong></a></li>
+                    <li><a href="/sobre"><strong>Sobre a PetAdota</strong></a></li>
+                    <li><a href="/formulario-cadastro"><strong>Cadastrar</strong></a></li>
+                </ul>
+            </nav>
         </header>
     );
 }
